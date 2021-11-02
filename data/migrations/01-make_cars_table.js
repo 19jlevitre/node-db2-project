2c3c25cb7b728107f7a37313a015e0ae7e039910
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-02 16:26:47 -0600

    data base is made with all keys and columns

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,20 @@
+
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+return knex.schema.createTable('cars', table => {
+  table.increments('id')
+
+  table.text('vin', 128).notNullable()
+
+  table.text('model', 128).notNullable()
+
+  table.integer('mileage').notNullable()
+
+  table.text('title', 128)
+
+  table.text('transmission', 128)
+})
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
